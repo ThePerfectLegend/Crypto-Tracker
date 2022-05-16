5cbf3e5d2898332a19fac1e19d0165a25fa31d53
Nizami Tagiyev <nizamion24s@gmail.com>
2022-05-16 19:42:53 +0500

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Nizami Tagiyev on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
